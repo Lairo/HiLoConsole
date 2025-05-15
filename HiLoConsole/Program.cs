92b1c05ab73b6affa87995dc295602f9f206d8e1
chajus1 <chajus1@googlemail.com>
2025-05-15 19:11:06 +0100

    testing referencing

--- HiLoConsole/Program.cs
+++ HiLoConsole/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Dog spot = new() { Age = 12, Name = "Spot" };
+            Dog larry = new() { Age = 7, Name = "Larry" };
+            Dog suzie = new() { Age = 4, Name = "Suzie" };
+
+            spot.TalkToMe();
+            larry.TalkToMe();
+            suzie.TalkToMe();
+            Console.WriteLine();
+
+            spot.Name = larry.Name;
+            larry.TalkToMe();
+            spot.TalkToMe();
+            Console.WriteLine();
+
+            spot = suzie;
+            Console.WriteLine();
+
+            suzie.TalkToMe();
+            spot.TalkToMe();
+            Console.WriteLine();
+
+            suzie.Name = "babe";
+            spot.TalkToMe();
+
+        }
+    }
+
+    public class Dog()
+    {
+        public string Name = "";
+        public int Age;
+
+        public void TalkToMe()
+        {
+            Console.WriteLine($"My name is {Name} and my age is {Age}. ");
         }
     }
 }
