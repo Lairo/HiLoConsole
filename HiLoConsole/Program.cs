473d9ce1a10dc5cfbf1dd815d293be73b012ffef
chajus1 <chajus1@googlemail.com>
2025-05-15 18:38:24 +0100

    Add project files.

---
+++ HiLoConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HiLoConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
